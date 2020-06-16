e52f5f9d0cbfe623b2fda14a8093b0e50a51c3d3
Tushar <tushar.mohan2001@gmail.com>
2020-06-17 03:13:20 +0530

    try

--- services/decrypt.py
+++ services/decrypt.py
@@ -19,20 +19,20 @@ def decrypt1(file_in, password,file_out):
     #file_in='C:\\Users\\Tushar\\projects\\django\\cipher\\cipher'+file_in
     file_in=MEDIA_ROOT+'/'+file_in.strip('/media/')
     #file_in='https://dipher.herokuapp.com'+file_in
-    #try:
-    document_in = PdfFileReader(open(file_in, 'rb'))
-    if document_in.isEncrypted:
-        while True:
-            matched = document_in.decrypt(password)
-            if matched:
-                document_out = PdfFileWriter()
-                document_out.cloneReaderDocumentRoot(document_in)
-                #tmp_file = tools.create_temp_file()
-                document_out.write(open(file_out, 'wb'))
-                shutil.copy(file_out, file_in)
-                os.remove(file_out)
+    try:
+        document_in = PdfFileReader(open(file_in, 'rb'))
+        if document_in.isEncrypted:
+            while True:
+                matched = document_in.decrypt(password)
+                if matched:
+                    document_out = PdfFileWriter()
+                    document_out.cloneReaderDocumentRoot(document_in)
+                    #tmp_file = tools.create_temp_file()
+                    document_out.write(open(file_out, 'wb'))
+                    shutil.copy(file_out, file_in)
+                    os.remove(file_out)
+                    return True
                 return True
-            return True
-    #except:
-    #    return False 
+    except:
+        return False 
     return False
--- services/encrypt.py
+++ services/encrypt.py
@@ -17,17 +17,17 @@ def encrypt(content):
         return s
 def encrypt1(file_in, password, file_out):
         #file_in='C:\\Users\\Tushar\\projects\\django\\cipher\\cipher'+file_in
-        #try:
-        file_in=MEDIA_ROOT+'/'+file_in.strip('/media/')
-        #file_in='https://dipher.herokuapp.com'+file_in
-        document_in = PdfFileReader(open(file_in, 'rb'))
-        document_out = PdfFileWriter()
-        document_out.cloneReaderDocumentRoot(document_in)
-        document_out.encrypt(password)
-        tmp_file=TemporaryFile()
-        #tmp_file = tools.create_temp_file()
-        document_out.write(open(file_out, 'wb'))
-        shutil.copy(file_out, file_in)
-        os.remove(file_out)
-        #except :
-        #    pass
+        try:
+                file_in=MEDIA_ROOT+'/'+file_in.strip('/media/')
+                #file_in='https://dipher.herokuapp.com'+file_in
+                document_in = PdfFileReader(open(file_in, 'rb'))
+                document_out = PdfFileWriter()
+                document_out.cloneReaderDocumentRoot(document_in)
+                document_out.encrypt(password)
+                tmp_file=TemporaryFile()
+                #tmp_file = tools.create_temp_file()
+                document_out.write(open(file_out, 'wb'))
+                shutil.copy(file_out, file_in)
+                os.remove(file_out)
+        except :
+            pass
